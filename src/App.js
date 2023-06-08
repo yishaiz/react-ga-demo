7bde73edf0cecfd8e2e2ef2bb9a88e06bca13c0b
yishaiza <Yishai.Zaltzberg@samanage.com>
2023-06-08 13:31:44 +0300

    add-input

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='title'>
-          Running Google Analytics (updated)
-        </p>
+        <p className="title">Running Google Analytics (updated)</p>
       </header>
+      <form>
+        <input type="text" name="myInput" />
+      </form>
     </div>
   );
 }
