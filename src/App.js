5f05a52aa7672d97c261d5a9e83832b228a29a7f
yishaiza <Yishai.Zaltzberg@samanage.com>
2023-06-08 15:33:49 +0300

    version

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const version = '1.1.1.1';
+  const version = '1.1.1.2';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="title">Running Google Analytics (updated)</p>
+        <p className="title">Running Google Analytics</p>
       </header>
       <p>version : {version}</p>
       <form>
