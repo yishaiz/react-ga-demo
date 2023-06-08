f2342038c4a8334900b2dd6be5b499569134126d
yishaiza <Yishai.Zaltzberg@samanage.com>
2023-06-08 13:18:22 +0300

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='title'>
           Running Google Analytics (updated)
         </p>
       </header>
