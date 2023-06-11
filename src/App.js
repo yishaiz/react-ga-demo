a882d5c8502cf5881e2a41129d622142a07a76f9
yishai <yishai5555@gmail.com>
2023-06-11 10:07:34 +0300

    add-modal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
+import { useState } from 'react';
 
 function App() {
-  const version = '1.1.1.2';
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+  console.log('render');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="title">Running Google Analytics</p>
-      </header>
-      <p>version : {version}</p>
-      <form>
-        <input type="text" name="myInput" />
-      </form>
+      <h1>React Animations</h1>
+      <Modal show={modalIsOpen} closed={() => closeModal()} />
+      <Backdrop show={modalIsOpen} />
+      <button className="Button" onClick={() => openModal()}>
+        Open Modal
+      </button>
+      <h3>Animating Lists</h3>
+      <List />
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p className="title">Running Google Analytics</p>
+    //   </header>
+    //   <p>version : {version}</p>
+    //   <form>
+    //     <input type="text" name="myInput" />
+    //   </form>
+    // </div>
   );
 }
 
