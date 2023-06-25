574cb7045730405a8a3803d8be3c514eb3c74030
yishaiza <Yishai.Zaltzberg@samanage.com>
2023-06-25 10:58:41 +0300

    update2

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard update with Jeff</div>;
+  return <div>Dashboard update with Jeff + update</div>;
 };
 
 export default Dashboard;
