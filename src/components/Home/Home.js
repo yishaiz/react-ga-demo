c375ff353304c1e0553e5e1f438f58843aca50b4
yishaiza <Yishai.Zaltzberg@samanage.com>
2023-06-28 10:21:48 +0300

    no_gtat

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import './Home.css';
-// import Modal from '../Modal/Modal';
-// import Backdrop from '../Backdrop/Backdrop';
 import List from '../List/List';
-// import { useState } from 'react';
 
 const Home = () => {
-  // const [modalIsOpen, setModalIsOpen] = useState(false);
-  // const openModal = () => setModalIsOpen(true);
-  // const closeModal = () => setModalIsOpen(false);
-  console.log('render');
+  // console.log('render');
   return (
     <div className="App">
       <h1>React Animations List</h1>
-      {/* <Modal show={modalIsOpen} closed={() => closeModal()} /> */}
-      {/* <Backdrop show={modalIsOpen} /> */}
-      {/* <button className="Button" onClick={() => openModal()}>
-        Open Modal
-      </button> */}
-      {/* <h3>Animating Lists</h3> */}
       <List />
     </div>
   );
 };
 
 export default Home;
+
+// import Modal from '../Modal/Modal';
+// import Backdrop from '../Backdrop/Backdrop';
+// import { useState } from 'react';
+
+// const [modalIsOpen, setModalIsOpen] = useState(false);
+// const openModal = () => setModalIsOpen(true);
+// const closeModal = () => setModalIsOpen(false);
+/*
+<Modal show={modalIsOpen} closed={() => closeModal()} /> 
+<Backdrop show={modalIsOpen} />
+ <button className="Button" onClick={() => openModal()}>
+  Open Modal
+</button> 
+   <h3>Animating Lists</h3> 
+  
+*/
