066cd1722dfabae35602aaa54bbbaf60e672285a
yishaiza <Yishai.Zaltzberg@samanage.com>
2023-06-28 10:43:50 +0300

    update_gtag_id_example2

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard update with Jeff + update</div>;
+  return <div>Dashboard (G-XFQHT4TXGG)</div>;
 };
 
 export default Dashboard;
