150b6b026837a2c94c80a6a63ad705b445a4b85b
yishaiza <Yishai.Zaltzberg@samanage.com>
2023-06-28 10:18:01 +0300

    add_item_event_window

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './Home.css';
-import Modal from '../Modal/Modal';
-import Backdrop from '../Backdrop/Backdrop';
+// import Modal from '../Modal/Modal';
+// import Backdrop from '../Backdrop/Backdrop';
 import List from '../List/List';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 const Home = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const openModal = () => setModalIsOpen(true);
-  const closeModal = () => setModalIsOpen(false);
+  // const [modalIsOpen, setModalIsOpen] = useState(false);
+  // const openModal = () => setModalIsOpen(true);
+  // const closeModal = () => setModalIsOpen(false);
   console.log('render');
   return (
     <div className="App">
       <h1>React Animations</h1>
-      <Modal show={modalIsOpen} closed={() => closeModal()} />
-      <Backdrop show={modalIsOpen} />
+      {/* <Modal show={modalIsOpen} closed={() => closeModal()} /> */}
+      {/* <Backdrop show={modalIsOpen} /> */}
       {/* <button className="Button" onClick={() => openModal()}>
         Open Modal
       </button> */}
